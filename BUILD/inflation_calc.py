435c8a375d8be1cf61c341ade140132b0c7494ec
Benjamin Himes <benjamindhimes@gmail.com>
2022-07-14 00:04:48 -0400

    Directory Structure Broke the directory structure down a bit because I've been drinking.

--- BUILD/inflation_calc.py
+++ BUILD/inflation_calc.py
@@ -5,20 +5,20 @@ import htmlmin
 from jinja2 import Environment, FileSystemLoader
 import jsmin
 
-DIR = pathlib.Path(__file__).parent.resolve()
+DIR = pathlib.Path(pathlib.Path(__file__).parent.parent, 'src')
 TEMPLATE_FILE = "inflation_calc.html"
 
 
 def main():
     env = Environment(
-        loader=FileSystemLoader(searchpath=DIR)
+        loader=FileSystemLoader(searchpath=DIR.resolve())
     )
     template = env.get_template(TEMPLATE_FILE)
     with open(pathlib.Path(DIR, "inflation_calc.js")) as js_f:
         inflation_js = jsmin.jsmin(js_f.read())
     rendered = template.render(inflation_js=inflation_js)
     minified = htmlmin.minify(rendered, remove_comments=True)
-    with open(pathlib.Path(DIR, "rendered.html"), "w+") as f:
+    with open(pathlib.Path(DIR.parent, "rendered.html"), "w+") as f:
         f.write(minified)
 
 
--- README.md
+++ README.md
@@ -3,7 +3,10 @@ Single-page HTML/JS that shows how badly inflation has wrecked your income
 
 To render the output file ('rendered.html', git ignored), run
 ```sh
-python3 inflation_calc.py
+./BUILD/inflation_calc.py
 ```
 
-This will render the HTML/JS into a single file and minify the output.
+This will render the HTML/JS/CSS into a single minified file in the root directory.
+
+
+Note that you may need to `pip install -r BUILD/requirements.txt` first.
