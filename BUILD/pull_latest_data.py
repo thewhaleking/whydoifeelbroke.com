b0b62e971ee81f6b6055d5ecd285393461185fee
Benjamin Himes <benjamindhimes@gmail.com>
2022-07-18 23:35:18 -0400

    Python function for retrieving latest CPI data.

---
+++ BUILD/pull_latest_data.py
@@ -0,0 +1,18 @@
+import requests
+
+
+def get_latest() -> float:
+    def line_key(line: str):
+        y = line.split('\t')
+        if 'M' in y[2]:
+            return y[1] + y[2]
+        else:
+            return y[1]
+
+    r = requests.get("https://download.bls.gov/pub/time.series/cu/cu.data.1.AllItems")
+    latest = max(r.text.splitlines()[1:], key=lambda x: line_key(x))
+    return float(latest.split('\t')[3].strip())
+
+
+if __name__ == "__main__":
+    print(get_latest())
