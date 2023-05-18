ef7ba6519aa5abb631222dbd07f927e62df0b338
Benjamin Himes <bhimes@aurora.tech>
2023-05-17 22:32:09 -0400

    Added a User Agent to get around the BLS's new policy of excluding bots.

--- BUILD/pull_latest_data.py
+++ BUILD/pull_latest_data.py
@@ -9,7 +9,12 @@ def get_latest() -> float:
         else:
             return y[1]
 
-    r = requests.get("https://download.bls.gov/pub/time.series/cu/cu.data.1.AllItems")
+    r = requests.get(
+        "https://download.bls.gov/pub/time.series/cu/cu.data.1.AllItems",
+        headers={
+              "User-Agent": 'Mozilla/4.0 (X11; MSIE 6.0; i686; .NET CLR 1.1.4322; .NET CLR 2.0.50727; FDM)'
+        }
+    )
     latest = max(r.text.splitlines()[1:], key=line_key)
     return float(latest.split('\t')[3].strip())
 
