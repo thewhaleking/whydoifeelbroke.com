026685b4d62c184757bad166ba54088b2529199e
bhimes <benjamindhimes@gmail.com>
2023-10-24 11:31:21 +0200

    Remove Python dependencies, switch to Node, update README to reflect this.

---
+++ BUILD/inflationCalc.js
@@ -0,0 +1,49 @@
+#!/usr/bin/env node
+
+const path = require('path');
+const { readFileSync, writeFileSync } = require('fs');
+const htmlmin = require('html-minifier').minify;
+const jsmin = require('jsmin').jsmin;
+const nunjucks = require('nunjucks');
+const postcss = require('postcss');
+const tailwindcss = require('tailwindcss');
+const cssnano = require('cssnano');
+
+const DIR = path.join(__dirname, '..');
+const TEMPLATE_FILE = "src/inflation_calc.html";
+
+async function compileTailwind(inputPath, outputPath) {
+    const inputCss = readFileSync(inputPath, 'utf8');
+    const output = await postcss([tailwindcss, cssnano])
+        .process(inputCss, {
+            from: inputPath,
+            to: outputPath
+        });
+    writeFileSync(outputPath, output.css);
+}
+
+async function main() {
+    // Set up Nunjucks environment
+    const env = new nunjucks.Environment(new nunjucks.FileSystemLoader(DIR, { noCache: true }));
+
+    await compileTailwind('./src/input.css', './dist/style.css')
+
+    const jsTemplate = env.render("src/inflation_calc.js");
+    const rendered = env.render(TEMPLATE_FILE, { inflation_js: jsTemplate });
+
+    const minifiedOptions = {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        removeOptionalTags: true,
+        minifyJS: true
+    };
+
+    const minified = htmlmin(rendered, minifiedOptions);
+
+    writeFileSync(path.join(DIR, "rendered.html"), minified);
+}
+
+main();
--- README.md
+++ README.md
@@ -1,12 +1,17 @@
 # whydoifeelbroke.com
 Single-page HTML/JS that shows how badly inflation has wrecked your income
 
+Node/NPM is required for this.
+
+
+To install the dependencies:
+```sh
+npm install
+```
+
 To render the output file ('rendered.html', git ignored), run
 ```sh
-./BUILD/inflation_calc.py
+./BUILD/inflationCalc.js
 ```
 
 This will render the HTML/JS/CSS into a single minified file in the root directory.
-
-
-Note that you may need to `pip install -r BUILD/requirements.txt` first.
